e1d975ed1ad135946790a330994e941288f43bbc
iurych <iury.vibe@gmail.com>
2023-05-20 13:37:06 -0300

    feat: add buttons to toolsbars

--- src/shared/components/details-tools/DetailsTools.tsx
+++ src/shared/components/details-tools/DetailsTools.tsx
@@ -1,10 +1,61 @@
-import { Box } from "@mui/material"
-
+import { Box, Button, Divider, Icon, Paper, useTheme } from '@mui/material';
 
 export const DetailsTools = () => {
-    return (
-        <Box>
-            toolsDetails
-        </Box>
-    )
-}
+  const theme = useTheme();
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display='flex'
+      alignItems='center'
+      gap={1}
+      component={Paper}
+    >
+      <Button
+        color='primary'
+        disableElevation
+        variant='contained'
+        startIcon={<Icon> save </Icon>}
+      >
+        Salvar
+      </Button>
+      <Button
+        color='primary'
+        disableElevation
+        variant='outlined'
+        startIcon={<Icon> save </Icon>}
+      >
+        Salvar e voltar
+      </Button>
+      <Button
+        color='primary'
+        disableElevation
+        variant='outlined'
+        startIcon={<Icon> delete </Icon>}
+      >
+        Apagar
+      </Button>
+      <Button
+        color='primary'
+        disableElevation
+        variant='outlined'
+        startIcon={<Icon> add </Icon>}
+      >
+        Novo
+      </Button>
+
+      <Divider variant='middle' orientation='vertical' />
+
+      <Button
+        color='primary'
+        disableElevation
+        variant='outlined'
+        startIcon={<Icon> arrow_back </Icon>}
+      >
+        Voltar
+      </Button>
+    </Box>
+  );
+};
